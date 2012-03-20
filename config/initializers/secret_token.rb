e42f645e3e7a1e721f885bbc06796dd430d24fd6
tomslinnesexitclear;backquit <toms.linnes@gmal.com>
2012-03-20 16:06:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lighteningtalk::Application.config.secret_token = '74a31a131f54ce3ba684399c643d7eda499d13467580399c70b1af82ac914f7483b15e99cdf05a4f3884b27ea45e6e6402d8f2fb8a83fd5be02ae3ced8806b3c'
